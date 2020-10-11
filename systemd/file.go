59fd981f7295776047487889717cbe5a649e6f8d
Nico Schieder <code@nico-schieder.de>
2020-10-11 21:39:14 +0200

    Add helper function

--- systemd/file.go
+++ systemd/file.go
@@ -20,10 +20,32 @@ type File struct {
 	Sections []Section
 }
 
+func (f *File) SectionsByName(name string) (out []Section) {
+	for _, section := range f.Sections {
+		if section.Name != name {
+			continue
+		}
+
+		out = append(out, section)
+	}
+	return
+}
+
 type Section struct {
 	Name    string
 	Comment string
 	Keys    []Key
+}
+
+func (s *Section) KeysByName(name string) (out []Key) {
+	for _, key := range s.Keys {
+		if key.Name != name {
+			continue
+		}
+
+		out = append(out, key)
+	}
+	return
 }
 
 type Key struct {
